1e966594fcaf0f06b59c9427a1e36ef4bce46186
Ashutosh Kumar <itsray650@gmail.com>
2025-12-02 10:17:51 +0530

    cors issue

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
-const port = 3002;
-
+// const port = 3002;
+import cors from "cors";
 
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
 
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+
+
+app.use(cors({
+    origin: function (origin, callback) {
+        console.log("REQUEST ORIGIN:", origin);
+        console.log("ENV CLIENT_URL:", process.env.CLIENT_URL);
+
+        const allowed = [
+            "http://localhost:3000",
+            "https://booksalon.vercel.app"
+        ];
+
+        if (!origin || allowed.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Blocked by CORS"));
+        }
+    },
+    credentials: true
+}));
